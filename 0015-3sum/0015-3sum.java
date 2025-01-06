53aa3ea0418dc3a3e02a7cd8397d2cbdf483c191
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-01-06 19:18:51 +0530

    Time: 31 ms (56.56%), Space: 52.8 MB (15.47%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums); // Sort the array
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    result.add(list);
+
+                    // Move j and k and avoid duplicates
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
