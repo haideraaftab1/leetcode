eaf74835f096d5b1d753d6f999409107aca76a37
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-01-11 12:09:36 +0530

    Time: 1 ms (84.87%), Space: 40.9 MB (53.04%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -5,11 +5,9 @@ class Solution {
         while (x != 0) {
             int lastDigit = x % 10;
 
-            // Check for overflow/underflow
             if (res < Integer.MIN_VALUE / 10 || res > Integer.MAX_VALUE / 10) {
-                return 0; // Return 0 if the result overflows
+                return 0; 
             }
-
             res = res * 10 + lastDigit;
             x = x / 10;
         }
